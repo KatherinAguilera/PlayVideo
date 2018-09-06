d06d18a9f5f57729dbfcebcb2e3cd47ac208b061
KatherinAguilera <kattyromance_17@hotmail.com>
2018-09-06 13:29:26 -0500

    Like animado

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Menu responsive
 (function(){
 	let movil = document.getElementById('movil');
 	let listaNav = document.getElementById('lista-nav');
@@ -6,3 +7,13 @@
 		listaNav.style.display = listaNav.style.display == 'flex'? 'none' : 'flex';
 	});
 })();
+
+//Animacion para likes
+
+    const $likestart = document.getElementById('likestart');
+    $likestart.addEventListener('click', (event)=>{
+      // console.log(event);
+      // $like.classList.add('is-liked');
+      $likestart.classList.toggle('is-liked2');
+      // $likestar.classList.toggle('is-liked2');
+    });
